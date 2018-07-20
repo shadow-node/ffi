12582645e4b99e9443545d87b9a4ed1523b76613
Lucas Woo <legendecas@gmail.com>
2018-07-20 18:38:25 +0800

    chore: search shadow-node headers in cmake include path

--- cmake/shadow_ffi.cmake
+++ cmake/shadow_ffi.cmake
@@ -2,6 +2,7 @@ target_link_libraries(shadow_ffi libffi)
 include_directories(
   include
   ${CMAKE_INCLUDE_PATH}
+  ${CMAKE_INCLUDE_PATH}/shadow-node
   ${PROJECT_SOURCE_DIR}/deps/libffi/include
   ${CMAKE_BINARY_DIR}/deps/libffi/include)
 

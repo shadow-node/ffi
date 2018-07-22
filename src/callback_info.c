96c2ae8b2d7c52c60a342a1f6c4f586d111f35bf
Lucas Woo <legendecas@gmail.com>
2018-07-22 19:16:36 +0800

    refactor: extract callback info from invoking JS callback

--- src/callback_info.c
+++ src/callback_info.c
@@ -16,32 +16,45 @@ static const jerry_object_native_info_t closure_pointer_object_type_info = {
   .free_cb = native_closure_pointer_free_cb
 };
 
-void sdf_dispatchToJs(sdffi_callback_info_t *info, void *retval, void **parameters) {
-  jerry_value_t jval_callback = info->callback;
-  ffi_cif *cif_ptr = info->cif;
-  jerry_value_t jargs[cif_ptr->nargs];
-  for (unsigned idx = 0; idx < cif_ptr->nargs; idx ++) {
-    jargs[idx] = wrap_ptr(parameters[idx]);
-  }
+jerry_value_t sdf_dispatchToJs(const jerry_value_t jval_callback,
+                               jerry_value_t *jargs,
+                               unsigned nargs) {
   jerry_value_t jval_func_this = jerry_create_null();
-  jerry_value_t jval_ret = jerry_call_function(jval_callback, jval_func_this, jargs, cif_ptr->nargs);
+  jerry_value_t jval_ret = jerry_call_function(jval_callback, jval_func_this, jargs, nargs);
   jerry_release_value(jval_func_this);
 
-  for (unsigned idx = 0; idx < cif_ptr->nargs; idx ++) {
-    jerry_release_value(jargs[idx]);
-  }
   if (jerry_value_has_error_flag(jval_ret)) {
     jerry_value_t jval_err_str = jerry_value_to_string(jval_ret);
     jerry_size_t size = jerry_get_utf8_string_size(jval_err_str);
     char err_str_buf[size];
     sdffi_copy_string_value(err_str_buf, jval_err_str);
-    printf("ffi: unexpected error on invoking js callback, reason: %s\n", err_str_buf);
+    printf("ffi: unexpected error on invoking js callback, reason: %s.\n", err_str_buf);
 
     jerry_release_value(jval_err_str);
   }
 
-  sdffi_cast_jval_to_pointer(retval, cif_ptr->rtype, jval_ret);
+  return jval_ret;
+}
 
+void sdf_dispatchToJsWithFFITypes(const jerry_value_t jval_callback,
+                                  ffi_type *rtype,
+                                  ffi_type **arg_types,
+                                  unsigned nargs,
+                                  void *retval,
+                                  void **parameters) {
+  jerry_value_t jargs[nargs];
+  for (unsigned idx = 0; idx < nargs; idx ++) {
+    jargs[idx] = wrap_ptr(parameters[idx]);
+  }
+
+  jerry_value_t jval_ret = sdf_dispatchToJs(jval_callback, jargs, nargs);
+  if (!jerry_value_has_error_flag(jval_ret)) {
+  sdffi_cast_jval_to_pointer(retval, rtype, jval_ret);
+  }
+
+  for (unsigned idx = 0; idx < nargs; idx ++) {
+    jerry_release_value(jargs[idx]);
+  }
   jerry_release_value(jval_ret);
 }
 
@@ -52,7 +65,14 @@ void sdf_dispatchToJs(sdffi_callback_info_t *info, void *retval, void **paramete
 
 void ffiInvoke(ffi_cif *cif, void *retval, void **parameters, void *user_data) {
   sdffi_callback_info_t *info = (sdffi_callback_info_t *)user_data;
-  sdf_dispatchToJs(info, retval, parameters);
+  jerry_value_t jval_callback = info->callback;
+
+  sdf_dispatchToJsWithFFITypes(jval_callback,
+                   cif->rtype,
+                   cif->arg_types,
+                   cif->nargs,
+                   retval,
+                   parameters);
 }
 
 JS_FUNCTION(WrapCallback) {
@@ -62,18 +82,18 @@ JS_FUNCTION(WrapCallback) {
   ffi_status status;
   ffi_closure *closure;
   void **code_loc = malloc(sizeof(void *));
-  sdffi_callback_info_t *user_data = malloc(sizeof(sdffi_callback_info_t));
-  user_data->cif = callback_cif;
-  user_data->code_loc = code_loc;
-  // TODO: user_data->callback shall be acquired for preventing it from being gc-ed
-  user_data->callback = jval_callback;
+  sdffi_callback_info_t *callback_info = malloc(sizeof(sdffi_callback_info_t));
+  callback_info->cif = callback_cif;
+  callback_info->code_loc = code_loc;
+  // TODO: callback_info->callback shall be acquired for preventing it from being gc-ed
+  callback_info->callback = jval_callback;
 
   closure = ffi_closure_alloc(sizeof(ffi_closure), code_loc);
-  status = ffi_prep_closure_loc(closure, callback_cif, ffiInvoke, user_data, *code_loc);
+  status = ffi_prep_closure_loc(closure, callback_cif, ffiInvoke, callback_info, *code_loc);
   if (status != FFI_OK) {
-    jerry_release_value(user_data->callback);
-    free(user_data->code_loc);
-    free(user_data);
+    jerry_release_value(callback_info->callback);
+    free(callback_info->code_loc);
+    free(callback_info);
     ffi_closure_free(closure);
     return jerry_create_number(status);
   }

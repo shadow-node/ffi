642b649732acd66acf35b9d159e6388193f13919
Lucas Woo <legendecas@gmail.com>
2018-07-23 14:25:52 +0800

    chore: cmake build type & sysroot support

--- cmake/shadow_ffi.cmake
+++ cmake/shadow_ffi.cmake
@@ -1,6 +1,9 @@
 target_link_libraries(shadow_ffi libffi)
 include_directories(
   include
+  ${CMAKE_SYSROOT}/include
+  ${CMAKE_SYSROOT}/usr/include
+  ${CMAKE_SYSROOT}/usr/include/shadow-node
   ${CMAKE_INCLUDE_PATH}
   ${CMAKE_INCLUDE_PATH}/shadow-node
   ${PROJECT_SOURCE_DIR}/deps/libffi/include

1f189aa6d8ec57939e7e089c7a311ffbc5e12571
Lucas Woo <legendecas@gmail.com>
2018-07-24 17:58:33 +0800

    chore: add option to use external libffi dynamic library

--- cmake/shadow_ffi.cmake
+++ cmake/shadow_ffi.cmake
@@ -1,4 +1,10 @@
-target_link_libraries(shadow_ffi libffi)
+if (LIBFFI_LINK_EXTERNAL)
+  find_library(LIBFFI_EXTERNAL_PATH NAMES libffi libffi.dylib libffi.so)
+  target_link_libraries(shadow_ffi ${LIBFFI_EXTERNAL_PATH})
+else()
+  target_link_libraries(shadow_ffi libffi)
+endif()
+
 include_directories(
   include
   ${CMAKE_SYSROOT}/include
@@ -8,5 +14,3 @@ include_directories(
   ${CMAKE_INCLUDE_PATH}/shadow-node
   ${PROJECT_SOURCE_DIR}/deps/libffi/include
   ${CMAKE_BINARY_DIR}/deps/libffi/include)
-
-link_directories(${PROJECT_SOURCE_DIR} ${CMAKE_BINARY_DIR}/lib)
